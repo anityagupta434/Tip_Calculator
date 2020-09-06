aeb62bf82f84308871be6eef871ed56e97865ea5
anityagupta434 <66381939+anityagupta434@users.noreply.github.com>
2020-09-05 20:48:57 -0500

    Add files via upload

---
+++ tips_test.swift
@@ -0,0 +1,35 @@
+//
+//  tips_test.swift
+//  tips_test
+//
+
+//
+
+import UIKit
+import XCTest
+
+class tips_tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+       
+        self.measureBlock() {
+            
+        }
+    }
+    
+}
